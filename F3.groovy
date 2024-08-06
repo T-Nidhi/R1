b10f364d6982b947405ec19ccecf245a317ca0f3
T-Nidhi <147977165+T-Nidhi@users.noreply.github.com>
2024-08-06 11:58:50 +0530

    Update F3.groovy

--- F3.groovy
+++ F3.groovy
@@ -1,10 +1,12 @@
 pipeline {
-    agent { docker { image 'python:3.12.4-alpine3.20' } }
+    agent any
+
     stages {
-        stage('build') {
+        stage('Hello') {
             steps {
-                echo 'python --version'
+                echo 'Hello World'
             }
         }
     }
 }
+v
